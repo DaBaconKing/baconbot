a29d6ce36b3685683c56fcbf87b8bd3a91bd5645
DaBaconKing <121511641+DaBaconKing@users.noreply.github.com>
2025-10-06 17:40:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const app = express();
+
 require('dotenv').config();
 
 const client = new Client({
@@ -13,10 +16,26 @@ client.once('ready', () => {
 });
 });
 
+app.get('/', (req, res) => {
+  res.send('🥓 BaconBot is alive');
+});
+
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web server running');
+});
+
+client.login(process.env.DISCORD_TOKEN).catch(err => {
+  console.error('Login failed:', err);
+  process.exit(1);
+});
+
+
 client.on('messageCreate', msg => {
   if (msg.content === '!bacon') {
     msg.reply('Crispy and operational!');
   }
 });
-
-client.login(process.env.DISCORD_TOKEN);
